a3cd3a172bdb8e3886dec80ce8310d019ec30531
Rotimanchase <rotimanchase@gmail.com>
2026-01-05 19:52:33 +0100

    prod

--- client/src/scenes/breakdown/index.jsx
+++ client/src/scenes/breakdown/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box } from '@mui/material'
 import Header from '../../components/Header'
-import Breakdownchart from '../../components/Breakdownchart'
+import Breakdownchart from '../../components/BreakdownChart'
 
 const Breakdown = () => {
   return (
